b29c5469271fc1f81a6baf1faaf95d6ccfaa32f9
gh0stwizard <vitaliy.tokarev@gmail.com>
2016-04-11 16:43:46 +0300

    added loadavg

---
+++ loadavg.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int
+main (void)
+{
+    double avenrun[3];
+    int i;
+
+    if (getloadavg (avenrun, sizeof (avenrun) / sizeof (avenrun[0])) == -1)
+        return (1);
+
+    for (i = 0; i < (int)(sizeof (avenrun) / sizeof (avenrun[0])); i++)
+        if (i > 0)
+            printf (", %.2f", avenrun[i]);
+        else
+            printf ("%.2f", avenrun[i]);
+    printf ("\n");
+
+    return (0);
+}
